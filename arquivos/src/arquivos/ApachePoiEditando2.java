244c19e6be1bcdb9fb63521dab6026f827840489
Rapha <Rapha@PcdoRapha>
2022-09-12 22:04:58 -0300

    Aula 16.7

---
+++ arquivos/src/arquivos/ApachePoiEditando2.java
@@ -0,0 +1,34 @@
+package arquivos;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+public class ApachePoiEditando2 {
+	public static void main(String[] args) throws Exception {
+		File file = new File("C:\\Users\\Rapha\\git\\arquivosemjava\\arquivos\\src\\arquivos\\arquivo_excel.xls");
+		FileInputStream entrada = new FileInputStream(file);
+		HSSFWorkbook hssfWorkbook = new HSSFWorkbook(entrada); //prepara a entrada do arquivo xls excel
+		HSSFSheet planilha = hssfWorkbook.getSheetAt(0); //pega a planilha
+		Iterator<Row>linhaIterator=planilha.iterator();
+		while(linhaIterator.hasNext()) { //enquanto tiver linha
+			Row linha = linhaIterator.next(); //dados da pessoa na linha
+			int numeroCelulas = linha.getPhysicalNumberOfCells(); //quantidade de células
+			Cell cell = linha.createCell(numeroCelulas); //cria nova célula na linha
+			cell.setCellValue("5.789");
+		}
+		entrada.close();
+		FileOutputStream saida = new FileOutputStream(file);
+		hssfWorkbook.write(saida);
+		saida.flush();
+		saida.close();
+		System.out.println("Planilha atualizada");
+	}
+}
